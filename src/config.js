c394bbf678e7f3dd3ce39fc3a1f7235fa73e36a7
tuan-nguyen-egany <tuan.nguyen@egany.com>
2020-11-21 10:01:59 +0700

    Refactor

--- src/config.js
+++ src/config.js
@@ -6,12 +6,14 @@ module.exports = {
   pass: process.env.MONGO_ROOT_PASSWORD || '',
   host: process.env.MONGO_HOST || 'localhost',
   port: process.env.MONGO_PORT || 27017,
+
   isAutoBackup: process.env.IS_AUTO_BACKUP || 1,
-  isRemoveOldBackup: process.env.IS_REMOVE_OLD_BACKUP || 1,
-  keepLastDaysBackup: process.env.KEEP_LAST_DAYS_BACKUP || 7,
-  isForceBackup: process.env.IS_FORCE_BACKUP || 0,
-  autoBackupPath: path.join(__dirname, 'backup'),
   cronJobTime: process.env.CRON_JOB_TIME || '00 00 * * *',
+  autoBackupPath: path.join(__dirname, 'backup'),
+  isForceBackup: process.env.IS_FORCE_BACKUP || 0,
+
+  isRemoveOldBackup: process.env.IS_REMOVE_OLD_BACKUP || 1,
+  keepLastDaysBackup: process.env.KEEP_LAST_DAYS_BACKUP || 2,
 
   googleClientMail: process.env.GOOGLE_CLIENT_MAIL || '',
   googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY || '',
